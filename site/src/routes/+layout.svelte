6ec16016177c319b035325f805f8fda55ba80574
Peter Hillerström <peter.hillerstrom@gmail.com>
2026-02-13 16:56:55 +0200

    Cleanup layout

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -4,11 +4,12 @@
 
 	import Navbar from '$component/Navbar.svelte'
 	import Alerts from '$component/Alerts.svelte'
+
 	interface Props {
-		children?: import('svelte').Snippet;
+		children?: import('svelte').Snippet
 	}
 
-	let { children }: Props = $props();
+	let { children }: Props = $props()
 </script>
 
 <svelte:head>
